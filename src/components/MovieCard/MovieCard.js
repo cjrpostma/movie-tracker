1edf6b6bea29e0ae71d7a52df82f4dea9dff5ad5
Ryan Bahan <ryan@ryanbahan.com>
2020-04-06 17:03:35 -0600

    Add structure and styling

--- src/components/MovieCard/MovieCard.js
+++ src/components/MovieCard/MovieCard.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import './MovieCard.css'
 
-const MovieCard = () => <div>Hello World</div>;
+const MovieCard = () => (
+  <article className="movie-card">
+    <img src="https://image.tmdb.org/t/p/original//8WUVHemHFH2ZIP6NWkwlHWsyrEL.jpg" alt="" className="movie-card-image"/>
+    <div className="title-wrapper">
+      <p className="movie-card-title">Bloodshot</p>
+      <p className="movie-card-rating">5 <i class="fas fa-star"></i></p>
+    </div>
+  </article>);
 
 export default MovieCard;
