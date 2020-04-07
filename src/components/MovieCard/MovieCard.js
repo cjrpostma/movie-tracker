a00e43d9c49e1f4f25f4ba63ac01ce3dd08ee77d
Chris Postma <cjrpostma@gmail.com>
2020-04-06 20:07:52 -0600

    Fix MovieCard merge conflict

--- src/components/MovieCard/MovieCard.js
+++ src/components/MovieCard/MovieCard.js
@@ -7,26 +7,16 @@ const MovieCard = ({
   imageBackdrop,
   release,
   description,
-  avgRating
+  avgRating,
 }) => (
   <article className="movie-card">
-<<<<<<< HEAD
-    <img
-      src="https://image.tmdb.org/t/p/original//8WUVHemHFH2ZIP6NWkwlHWsyrEL.jpg"
-      alt=""
-      className="movie-card-image"
-    />
-    <div className="title-wrapper">
-      <p className="movie-card-title">Bloodshot</p>
-      <p className="movie-card-rating">
-        5 <i className="fas fa-star"></i>
-      </p>
-=======
-    <img src={image} alt={"Poster for " + title} className="movie-card-image"/>
+    <img src={image} alt={`Poster for ${title}`} className="movie-card-image" />
     <div className="title-wrapper">
       <p className="movie-card-title">{title}</p>
-      <p className="movie-card-rating">{avgRating}<i className="fas fa-star"></i></p>
->>>>>>> master
+      <p className="movie-card-rating">
+        {avgRating}
+        <i className="fas fa-star"></i>
+      </p>
     </div>
   </article>
 );
