196222e5b945c6726d41a629aa1574d00a101366
Chris Postma <cjrpostma@gmail.com>
2020-04-06 20:59:27 -0600

    Add conditional components Error, PageNotFound, Loader

---
+++ src/components/Error/Error.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './_Error.scss';
+
+const Error = props => <p className="error">{props.error}</p>;
+
+// Error.propTypes = {}
+
+export default Error;
