8e33dfeb5d5ddec10d60dca217c2bdd55465dc3b
Ryan Bahan <ryan@ryanbahan.com>
2020-04-06 18:07:53 -0600

    Add fetch

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,22 @@
 import React from 'react';
 import MovieCardContainer from '../MovieCardContainer/MovieCardContainer';
+import { fetchMovies } from '../../apiCalls/fetchMovies';
 
-const App = () => <div><MovieCardContainer /></div>;
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      movies: []
+    }
+  }
+
+  componentDidMount() {
+    fetchMovies().then(movies => this.setState({movies: movies}));
+  }
+
+  render() {
+    return <div><MovieCardContainer movies={this.state.movies}/></div>
+  }
+};
 
 export default App;
