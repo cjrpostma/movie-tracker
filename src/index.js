33922f1e1dc991d24125a7097d31293f6e971e99
Ryan Bahan <ryan@ryanbahan.com>
2020-04-07 16:59:09 -0600

    Add initial Redux structuring

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-// import { Provider } from 'react-redux';
-// import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import rootReducer from './reducers';
 import App from './components/App/App';
 import './index.css';
 
+const store = createStore(rootReducer, composeWithDevTools());
+
 render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('root')
 );
---
+++ src/thunks/authorizeUser.js
@@ -0,0 +1,5 @@
+import { loginUser } from "../actions";
+
+export const authorizeUser = () => {
+  
+}
