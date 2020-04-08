6071cdaf05a37fdb6d91b9af29f8c98b383460f5
Chris Postma <cjrpostma@gmail.com>
2020-04-08 13:49:23 -0600

    Add tests for fetchMovies api call

--- src/apiCalls/fetchMovies.test.js
+++ src/apiCalls/fetchMovies.test.js
@@ -1,34 +1,65 @@
 import { fetchMovies } from './fetchMovies';
 
 describe('fetchMovies', () => {
-  let mockResponse;
-
-  beforeEach(() => {
-    mockResponse = [
+  const mockResponse = {
+    movies: [
       {
         id: 1,
-        title: 'mockTitle',
-        poster_path: 'mockPosterPath',
-        backdrop_path: 'mockBackdropPath',
-        release_date: '2020-01-01',
-        overview: 'mockOverview',
-        average_rating: 10,
+        title: 'mockTitle1',
+        poster_path: 'mockPosterPath1',
+        backdrop_path: 'mockBackdropPath1',
+        release_date: 'mockReleaseDate1',
+        overview: 'mockOverview1',
+        average_rating: 1,
       },
-    ];
-
-    window.fetch = jest.fn().mockImplementation(() =>
-      Promise.resolve({
-        ok: true,
-        json: Promise.resolve(mockResponse),
-      })
-    );
-  });
+      {
+        id: 2,
+        title: 'mockTitle2',
+        poster_path: 'mockPosterPath2',
+        backdrop_path: 'mockBackdropPath2',
+        release_date: 'mockReleaseDate2',
+        overview: 'mockOverview2',
+        average_rating: 2,
+      },
+    ],
+  };
 
   it('should call fetch with the correct arguments', () => {
+    window.fetch = jest.fn();
+
     fetchMovies();
     expect(window.fetch).toHaveBeenCalledTimes(1);
     expect(window.fetch).toHaveBeenCalledWith(
       'https://rancid-tomatillos.herokuapp.com/api/v1/movies'
     );
+  });
+
+  it('should return the correct data', () => {
+    window.fetch = jest.fn().mockImplementation(() =>
+      Promise.resolve({
+        ok: true,
+        json: () => Promise.resolve(mockResponse),
+      })
+    );
+
+    expect(fetchMovies()).resolves.toEqual(mockResponse.movies);
+  });
+
+  it('should throw if response not ok', () => {
+    window.fetch = jest.fn().mockImplementation(() =>
+      Promise.resolve({
+        ok: false,
+      })
+    );
+
+    expect(fetchMovies()).rejects.toEqual(Error('Error fetching movies.'));
+  });
+
+  it('should reject when failing to fetch', () => {
+    window.fetch = jest
+      .fn()
+      .mockImplementation(() => Promise.reject(Error('Failed to fetch')));
+
+    expect(fetchMovies()).rejects.toEqual(Error('Failed to fetch'));
   });
 });
