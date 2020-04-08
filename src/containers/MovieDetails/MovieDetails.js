512abe61d6e0bb94b33abf3c071321a8f5f5f625
Ryan Bahan <ryan@ryanbahan.com>
2020-04-08 13:41:25 -0600

    Set up redux with movieDetails

--- src/containers/MovieDetails/MovieDetails.js
+++ src/containers/MovieDetails/MovieDetails.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import { requestMovies } from '../../actions';
 import './MovieDetails.scss';
 import Loader from '../../components/Loader/Loader';
 
-const MovieDetails = () => {
-  return <div>test</div>
+const MovieDetails = ({ match, movies}) => {
+  console.log(movies);
+  const getMovie = (movies) => {
+    const correctMovie = movies.find(movie => movie.id === parseInt(match.params.id));
+    console.log(correctMovie);
+    return <div></div>
+  }
+
+  return <div>{getMovie(movies)}</div>
 }
 
-const mapDispatchToProps = state => ({
+const mapStateToProps = state => ({
+  movies: state.movies
 });
 
-export default connect(null, mapDispatchToProps)(MovieDetails);
+export default connect(mapStateToProps)(MovieDetails);
