2022a3283613fafa479aa339ed18c9738bd4e7c9
Chris Postma <cjrpostma@gmail.com>
2020-04-07 21:41:46 -0600

    Fix bug with incorrectly named actionType for isLoading

--- src/actionTypes/index.js
+++ src/actionTypes/index.js
@@ -1,4 +1,4 @@
 export const HAS_ERRORED = 'HAS_ERRORED';
-export const IS_LOADING = 'LOGIN_USER';
+export const IS_LOADING = 'IS_LOADING';
 export const LOGIN_USER = 'LOGIN_USER';
 export const LOGOUT_USER = 'LOGOUT_USER';
