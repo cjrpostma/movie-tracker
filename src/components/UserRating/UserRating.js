11d5b7a1602150e2bd5698c5399bf3ec145e0834
Ryan Bahan <ryan@ryanbahan.com>
2020-04-09 14:43:57 -0600

    Add dropdown toggling

--- src/components/UserRating/UserRating.js
+++ src/components/UserRating/UserRating.js
@@ -5,14 +5,36 @@ class UserRating extends React.Component {
   constructor() {
     super();
     this.state = {
-      rating: null
+      rating: null,
+      dropdown: false
     }
   }
+
+  toggleDropdown = (e) => {
+    this.setState({dropdown: !this.state.dropdown})
+  }
+
   render() {
+    const dropdownHidden = this.state.dropdown ? "" : "hidden";
+    const dropdownClasses = `ratings-dropdown ${dropdownHidden}`;
+
     return (
-      <div className="user-rating">
+      <div className="user-rating" onClick={() => this.toggleDropdown()}>
         <p className="rating">{this.state.rating || "Rate This"}</p>
-        <i class="fas fa-thumbs-up"></i>
+        <i className="fas fa-thumbs-up"></i>
+        <div className={dropdownClasses}>
+          <i className="fas fa-thumbs-up" data-value="1"></i>
+          <i className="fas fa-thumbs-up" data-value="2"></i>
+          <i className="fas fa-thumbs-up" data-value="3"></i>
+          <i className="fas fa-thumbs-up" data-value="4"></i>
+          <i className="fas fa-thumbs-up" data-value="5"></i>
+          <i className="fas fa-thumbs-up" data-value="6"></i>
+          <i className="fas fa-thumbs-up" data-value="7"></i>
+          <i className="fas fa-thumbs-up" data-value="8"></i>
+          <i className="fas fa-thumbs-up" data-value="9"></i>
+          <i className="fas fa-thumbs-up" data-value="10"></i>
+          <i className="far fa-times-circle"></i>
+        </div>
       </div>)
   }
 }
