59f6eb4cdba7f8897fef0f700e455726b645556d
Ryan Bahan <ryan@ryanbahan.com>
2020-04-09 14:57:42 -0600

    Add color highlighter

--- src/components/UserRating/UserRating.js
+++ src/components/UserRating/UserRating.js
@@ -14,6 +14,17 @@ class UserRating extends React.Component {
     this.setState({dropdown: !this.state.dropdown})
   }
 
+  hoverHandler = (e) => {
+    const stars = e.target.parentElement.getElementsByClassName('rating-selector');
+    const hoverValue = e.target.dataset.value;
+
+    Array.from(stars).forEach(star => {
+      console.log('hover', hoverValue);
+      console.log('star', star.dataset.value);
+      star.style.color = hoverValue >= star.dataset.value ? "#ffdd00" : '#e4e4e4';
+    });
+  }
+
   render() {
     const dropdownHidden = this.state.dropdown ? "" : "hidden";
     const dropdownClasses = `ratings-dropdown ${dropdownHidden}`;
@@ -22,17 +33,17 @@ class UserRating extends React.Component {
       <div className="user-rating" onClick={() => this.toggleDropdown()}>
         <p className="rating">{this.state.rating || "Rate This"}</p>
         <i className="fas fa-thumbs-up"></i>
-        <div className={dropdownClasses}>
-          <i className="fas fa-thumbs-up" data-value="1"></i>
-          <i className="fas fa-thumbs-up" data-value="2"></i>
-          <i className="fas fa-thumbs-up" data-value="3"></i>
-          <i className="fas fa-thumbs-up" data-value="4"></i>
-          <i className="fas fa-thumbs-up" data-value="5"></i>
-          <i className="fas fa-thumbs-up" data-value="6"></i>
-          <i className="fas fa-thumbs-up" data-value="7"></i>
-          <i className="fas fa-thumbs-up" data-value="8"></i>
-          <i className="fas fa-thumbs-up" data-value="9"></i>
-          <i className="fas fa-thumbs-up" data-value="10"></i>
+        <div className={dropdownClasses} onMouseOver={this.hoverHandler}>
+          <i className="fas fa-thumbs-up rating-selector" data-value="0"></i>
+          <i className="fas fa-thumbs-up rating-selector" data-value="1"></i>
+          <i className="fas fa-thumbs-up rating-selector" data-value="2"></i>
+          <i className="fas fa-thumbs-up rating-selector" data-value="3"></i>
+          <i className="fas fa-thumbs-up rating-selector" data-value="4"></i>
+          <i className="fas fa-thumbs-up rating-selector" data-value="5"></i>
+          <i className="fas fa-thumbs-up rating-selector" data-value="6"></i>
+          <i className="fas fa-thumbs-up rating-selector" data-value="7"></i>
+          <i className="fas fa-thumbs-up rating-selector" data-value="8"></i>
+          <i className="fas fa-thumbs-up rating-selector" data-value="9"></i>
           <i className="far fa-times-circle"></i>
         </div>
       </div>)
