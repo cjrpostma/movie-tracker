a7956b08ac72df596faf049ca0a0744478727da2
Chris Postma <cjrpostma@gmail.com>
2020-04-14 14:37:33 -0600

    Updates

---
+++ precache-manifest.0d5bfdf829cded6bbd74e61ad3240a37.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "163e34d140d282537cc97ef81ea66ab1",
+    "url": "/movie-tracker/index.html"
+  },
+  {
+    "revision": "c1f6c8bc2b03cb50eaec",
+    "url": "/movie-tracker/static/css/main.b1293ca6.chunk.css"
+  },
+  {
+    "revision": "3af001a955d7cf888e72",
+    "url": "/movie-tracker/static/js/2.e3966561.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie-tracker/static/js/2.e3966561.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1f6c8bc2b03cb50eaec",
+    "url": "/movie-tracker/static/js/main.029d5fa8.chunk.js"
+  },
+  {
+    "revision": "08d4d526c731b53064a5",
+    "url": "/movie-tracker/static/js/runtime-main.765b5700.js"
+  }
+]);
